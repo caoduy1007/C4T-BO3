ec60f066877c704df94341ed08eeb84b98a536ba
lemoque <50577842+lemoque@users.noreply.github.com>
2019-06-09 12:01:42 +0700

    first commit

---
+++ session1.py
@@ -0,0 +1,8 @@
+# chieuCao = int(input("Nhap chieu cao: ")
+# canhDay = int(input("Nhap canh day: ")
+# dienTichTamGiac = (chieuCao * canhDay) * 0.5
+# print(dienTichTamGiac)
+
+banKinh = float(input("Nhap ban kinh: "))
+dienTichHinhTron = banKinh*3.14
+print(dienTichHinhTron)
